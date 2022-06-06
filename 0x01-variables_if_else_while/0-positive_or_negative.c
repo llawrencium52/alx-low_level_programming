3ff1454c04d09927a2780d71639908d092ba9943
llawrencium52 <105044731+llawrencium52@users.noreply.github.com>
2022-06-06 01:40:36 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,7 +10,6 @@
 int main(void)
 {
 	int n;
-	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
@@ -19,6 +18,5 @@ int main(void)
 		printf("%i is negative\n", n);
 	else
 		printf("%i is zero\n", n);
-
 	return (0);
 }
