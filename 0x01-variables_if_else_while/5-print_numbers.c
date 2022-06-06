02b2a1b9cf284038d6ac137fbecc80a92b16319a
llawrencium52 <105044731+llawrencium52@users.noreply.github.com>
2022-06-06 02:40:29 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,20 @@
-o
+#include <stdio.h>
+
+/**
+ *main - print 0123456789
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%i", n);
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
