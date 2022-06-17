e7eb631b20f8a757d64ed1fc95e1e9b498e3e38d
llawrencium52 <105044731+llawrencium52@users.noreply.github.com>
2022-06-17 16:44:50 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,31 @@
-#include "main.h"
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
- * main - check the code for ALX students.
- *
- * Return: Always 0.
+ *main - Entry point
+ *Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int n;
 
-	n = 402;
-	printf("n=%d\n", n);
-	reset_to_98(&n);
-	printf("n=%d\n", n);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%i is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%i is zero\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%i is negative\n", n);
+	}
+
 	return (0);
 }
